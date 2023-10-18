e5243fe6118916e3038cd3449ff85b5a9d9269d2
anton <anton@Laptop>
2023-10-18 16:50:10 -0500

    egresados, imprimir waza

--- src/Egresados/Egresados.java
+++ src/Egresados/Egresados.java
@@ -4,6 +4,7 @@ public class Egresados {
     public static void main(String[] args) {
         frmLogin f = new frmLogin();
         f.setVisible(true);
+        System.out.println("Waza");
     }
     
 }
